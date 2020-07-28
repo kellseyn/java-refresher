a78ae94dd6d66cf343f14fe5bd01315821f7c302
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-28 10:31:23 -0500

    fixed code bugs

--- src/refresher/java/Keywords.java
+++ src/refresher/java/Keywords.java
@@ -2,15 +2,52 @@ package refresher.java;
 
 public class Keywords {
     public static void main(String[] args) {
-        calculateScore(true, 800, 5,100);
-        calculateScore(true, 10000, 8,200);
+
+        int highScore = calculateScore(true, 800, 5,100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10000, 8,200);
+        System.out.println("Your final score was " + highScore);
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert", highScorePosition);
     }
 
-    public static void calculateScore (boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore (boolean gameOver, int score, int levelCompleted, int bonus){
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 2000;
+            return finalScore;
+        } else {
+            return -1;
+        }
+    }
+
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition){
+        System.out.println(playerName + " managed to get into postion " + highScorePosition + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int finalScore){
+
+        if (finalScore >= 1000){
+            return 1;
+        } else if (finalScore >= 500 && finalScore < 1000){
+            return 2;
+        } else if (finalScore >= 100 && finalScore < 500){
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
