08455f5205991b5d6e8e26b72df72bb44a1a51da
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-29 16:48:05 -0500

    added prime number loop counter

--- src/refresher/java/ForLoops.java
+++ src/refresher/java/ForLoops.java
@@ -9,8 +9,34 @@ public class ForLoops {
         for (double i = 2; i < 9; i++){
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
+        countPrimes();
     }
     public static double calculateInterest (double amount, double interest){
         return (amount * (interest/100));
+    }
+
+    public static boolean isPrime(int n){
+        if (n == 1){
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void countPrimes(){
+        int n = 0;
+        for (int i =1; i <= 100; i++){
+            if (isPrime(i)){
+                System.out.println("Prime number: " + i);
+                System.out.println("Prime number count: " + ++n);
+                if (n >= 10){
+                    break;
+                }
+            }
+        }
     }
 }
