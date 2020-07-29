3ccbd384e24220ce286fcb64848f5d7edbad34f9
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-29 16:18:20 -0500

    added on to the leap year exercise checking days in month based on leap year

--- src/refresher/java/LeapYear.java
+++ src/refresher/java/LeapYear.java
@@ -25,10 +25,37 @@ public class LeapYear {
         }
     }
 
+    public static int getDaysInMonth(int month, int year){
+        if (month < 1 || month > 12 || year < 1 || year > 9999){
+            return -1;
+        } else {
+            switch (month){
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    return 31;
+//                    break;
+                case 2:
+                    if (isLeapYear(year)){
+                        return 29;
+                    } else{
+                        return 28;
+                    }
+//                    break;
+                case 4: case 6: case 9: case 11:
+                    return 30;
+//                    break;
+
+                default:
+                    return -1;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(isLeapYear(-1600));
         System.out.println(isLeapYear(1600));
         System.out.println(isLeapYear(2017));
         System.out.println(isLeapYear(1924));
+        System.out.println(getDaysInMonth(2,2020));
+        System.out.println(getDaysInMonth(2,2015));
     }
 }
