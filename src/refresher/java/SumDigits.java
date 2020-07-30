9144c1a944f5c6a2d5412d564f6622f2e969b15c
Kellsey Neagley <kellseyneagley@gmail.com>
2020-07-30 14:38:02 -0500

    completed digit sum challenge

---
+++ src/refresher/java/SumDigits.java
@@ -0,0 +1,21 @@
+package refresher.java;
+
+
+public class SumDigits {
+
+    public static void main(String[]args){
+        System.out.println("sum of digits " + sumDigits(125));
+    }
+    public static int sumDigits(int number){
+        if (number < 10){
+           return -1;
+        }
+        int sum = 0;
+        while (number > 0){
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
