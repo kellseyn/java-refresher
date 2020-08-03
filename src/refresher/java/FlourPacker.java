63238d2be3cd617b637d3742f7247e40cc690757
Kellsey Neagley <kellseyneagley@gmail.com>
2020-08-03 10:47:18 -0500

    completed flourpacker exercise

---
+++ src/refresher/java/FlourPacker.java
@@ -0,0 +1,26 @@
+package refresher.java;
+
+public class FlourPacker {
+    public static void main(String[] args) {
+        System.out.println(canPack(1,0,4));
+        System.out.println(canPack(1,0,5));
+        System.out.println(canPack(0, 5 ,4));
+        System.out.println(canPack(2,2,11));
+        System.out.println(canPack(-3,2,12));
+    }
+    public static boolean canPack (int bigCount, int smallCount, int goal){
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0){
+            return false;
+        }
+        int bigBag = 0;
+
+        for(int i = 0; i <= bigCount; i++){
+            if ((i *  5) <= goal){
+                bigBag = i;
+            }
+        }
+        return ((bigBag * 5) + smallCount) >= goal;
+
+    }
+}
