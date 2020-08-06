f76723995a8ebff56ff262097543e7ef0f8019b6
Kellsey Neagley <kellseyneagley@gmail.com>
2020-08-06 15:41:40 -0500

    completed inheritance lesson part one

--- src/refresher/java/Main.java
+++ src/refresher/java/Main.java
@@ -11,14 +11,17 @@ public class Main {
 //        System.out.println("Porsche model: " + porsche.getModel());
 //        System.out.println("Holden model: " + holden.getModel());
 
-        BankAccount myAcct = new BankAccount(123456789, 500.00, "Kells Neagley", "kells@email.com", 2102102102);
-        System.out.println(myAcct.getBalance());
-        myAcct.depositFunds(75.75);
-        System.out.println(myAcct.getBalance());
-        myAcct.withdrawFunds(900.00);
-        System.out.println(myAcct.getBalance());
-        myAcct.withdrawFunds(400.00);
-        System.out.println(myAcct.getBalance());
+        //Bank account exercise
+//        BankAccount myAcct = new BankAccount(123456789, 500.00, "Kells Neagley", "kells@email.com", 2102102102);
+//        System.out.println(myAcct.getBalance());
+//        myAcct.depositFunds(75.75);
+//        System.out.println(myAcct.getBalance());
+//        myAcct.withdrawFunds(900.00);
+//        System.out.println(myAcct.getBalance());
+//        myAcct.withdrawFunds(400.00);
+//        System.out.println(myAcct.getBalance());
+
+        //Inheritance lesson
 
 
 
