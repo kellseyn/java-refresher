15f52d2f0ff3e606f5859843d879f970352900c2
Kellsey Neagley <kellseyneagley@gmail.com>
2020-08-18 15:16:27 -0500

    looking into node closer

--- src/refresher/java/Node.java
+++ src/refresher/java/Node.java
@@ -4,7 +4,24 @@ public class Node {
     private int value;
     private Node next;
 
-    public void setValue(int value){
-        this.value = value;
+    public Node(int v, Node nx) {
+        this.value = v;
+        this.next = nx;
+    }
+
+    public void setValue(int v){
+        this.value = v;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node nx) {
+        this.next = nx;
     }
 }
